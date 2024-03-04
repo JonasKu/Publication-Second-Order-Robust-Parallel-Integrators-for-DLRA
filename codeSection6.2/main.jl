4833f2d803c46e03d37c3a0164a7b36645d8cf58
Jonas Kusch <jonas.kusch@kit.edu>
2024-03-04 15:16:03 +0100

    make lattice test easier to run

--- codeSection6.2/main.jl
+++ codeSection6.2/main.jl
@@ -1,3 +1,7 @@
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+
 using Base: Float64
 include("utils.jl")
 include("settings.jl")
@@ -9,43 +13,43 @@ using DelimitedFiles
 close("all")
 
 nₚₙ = 21;
-ϑₚ = 1e-2;      # parallel tolerance
-ϑᵤ = 5e-2;    # (unconventional) BUG tolerance # BUG 0.05, parrallel 0.02 looks okay
-ϑₚₕ = 1e-2;      # parallel tolerance
-ϑᵤₕ = 3e-2;    # (unconventional) BUG tolerance
-
-s = Settings(251,251, nₚₙ, 20,"Lattice"); # create settings class with 351 x 351 spatial cells and a rank of 50
 
 ################################################################
 ######################### execute code #########################
 ################################################################
 
 ##################### classical checkerboard #####################
+s = Settings(251,251, nₚₙ, 10,"Lattice"); # create settings class with 251 x 251 spatial cells and a rank of 10
 
 ################### run full method ###################
 solver = SolverDLRA(s);
 @time rhoFull = Solve(solver);
 rhoFull = Vec2Mat(s.NCellsX,s.NCellsY,rhoFull)
 
-##################### low tolerance #####################
+##################### low rank #####################
 
-################### run BUG adaptive ###################
-s.ϑ = ϑᵤ;
+################### run midpoint BUG ###################
 solver = SolverDLRA(s);
 @time rhoDLRA,rankInTime = SolveBUG2nd(solver);
 rhoDLRA = Vec2Mat(s.NCellsX,s.NCellsY,rhoDLRA);
 
-################### run parallel ###################
-s.ϑ = ϑₚ;
-solver = SolverDLRA(s);
-@time rhoDLRAp,rankInTimep = SolveParallel(solver);
-rhoDLRAp = Vec2Mat(s.NCellsX,s.NCellsY,rhoDLRAp);
-
 ################### run parallel 2nd ###################
-s.ϑ = ϑₚ;
 solver = SolverDLRA(s);
 @time rhoDLRAp2,rankInTimep2 = SolveParallel2nd(solver);
 rhoDLRAp2 = Vec2Mat(s.NCellsX,s.NCellsY,rhoDLRAp2);
+
+##################### higher rank #####################
+s = Settings(251,251, nₚₙ, 20,"Lattice"); # create settings class with 251 x 251 spatial cells and a rank of 20
+
+################### run midpoint BUG ###################
+solver = SolverDLRA(s);
+@time rhoDLRAh,rankInTime = SolveBUG2nd(solver);
+rhoDLRAh = Vec2Mat(s.NCellsX,s.NCellsY,rhoDLRAh);
+
+################### run parallel 2nd ###################
+solver = SolverDLRA(s);
+@time rhoDLRAp2h,rankInTimep2 = SolveParallel2nd(solver);
+rhoDLRAp2h = Vec2Mat(s.NCellsX,s.NCellsY,rhoDLRAp2h);
 
 ############################################################
 ######################### plotting #########################
@@ -70,50 +74,63 @@ cbar = plt.colorbar(pc)
 cbar.ax.tick_params(labelsize=20)
 tight_layout()
 show()
-savefig("results/scalar_flux_PN_$(s.problem)_nx$(s.NCellsX)_N$(s.nₚₙ).eps")
+savefig("scalar_flux_PN_$(s.problem)_nx$(s.NCellsX)_N$(s.nₚₙ).eps")
 
 ## DLRA BUG adaptive
 idxNeg = findall((rhoDLRA.<=0.0))
 rhoDLRA[idxNeg] .= NaN;
-fig = figure("midpoint BUG, log, ϑ coarse",figsize=(9,10),dpi=100)
+fig = figure("midpoint BUG, log, lower rank",figsize=(9,10),dpi=100)
 ax = gca()
 pcolormesh(X,Y,4.0*pi*sqrt(2)*rhoDLRA[2:(end-1),(end-1):-1:2]',norm=matplotlib.colors.LogNorm(vmin=minV, vmax=maxV))
+ax.tick_params("both",labelsize=20) 
+plt.xlabel("x", fontsize=20)
+plt.ylabel("y", fontsize=20)
+plt.title(L"$\Phi$, midpoint BUG, $r = $ "*LaTeXString(string(10)), fontsize=25)
+tight_layout()
+show()
+savefig("scalar_flux_DLRA_adBUG_$(s.problem)_rank10_nx$(s.NCellsX)_N$(s.nₚₙ).png")
+
+## DLRA parallel 2nd order
+idxNeg = findall((rhoDLRAp2.<=0.0))
+rhoDLRAp2[idxNeg] .= NaN;
+fig = figure("parallel 2nd, log, lower rank",figsize=(9,10),dpi=100)
+ax = gca()
+pcolormesh(X,Y,4.0*pi*sqrt(2)*rhoDLRAp2[2:(end-1),(end-1):-1:2]',norm=matplotlib.colors.LogNorm(vmin=minV, vmax=maxV))
+ax.tick_params("both",labelsize=20) 
+plt.xlabel("x", fontsize=20)
+plt.ylabel("y", fontsize=20)
+plt.title(L"$\Phi$, parallel $2^{nd}$ order, $r = $ "*LaTeXString(string(10)), fontsize=25)
+tight_layout()
+show()
+savefig("scalar_flux_parallel2nd_$(s.problem)_rank10_nx$(s.NCellsX)_N$(s.nₚₙ).png")
+
+## DLRA BUG adaptive higher rank
+idxNeg = findall((rhoDLRAh.<=0.0))
+rhoDLRAh[idxNeg] .= NaN;
+fig = figure("midpoint BUG, log, higher rank",figsize=(9,10),dpi=100)
+ax = gca()
+pcolormesh(X,Y,4.0*pi*sqrt(2)*rhoDLRAh[2:(end-1),(end-1):-1:2]',norm=matplotlib.colors.LogNorm(vmin=minV, vmax=maxV))
 ax.tick_params("both",labelsize=20) 
 plt.xlabel("x", fontsize=20)
 plt.ylabel("y", fontsize=20)
 plt.title(L"$\Phi$, midpoint BUG, $r = $ "*LaTeXString(string(s.r)), fontsize=25)
 tight_layout()
 show()
-savefig("results/scalar_flux_DLRA_adBUG_$(s.problem)_rank$(s.r)_nx$(s.NCellsX)_N$(s.nₚₙ).png")
+savefig("scalar_flux_DLRA_adBUG_$(s.problem)_rank20_nx$(s.NCellsX)_N$(s.nₚₙ).png")
 
-## DLRA parallel
-idxNeg = findall((rhoDLRAp.<=0.0))
-rhoDLRAp[idxNeg] .= NaN;
-fig = figure("parallel, log, ϑ coarse",figsize=(9,10),dpi=100)
+## DLRA parallel 2nd order higher rank
+idxNeg = findall((rhoDLRAp2h.<=0.0))
+rhoDLRAp2h[idxNeg] .= NaN;
+fig = figure("parallel 2nd, log, higher rank",figsize=(9,10),dpi=100)
 ax = gca()
-pcolormesh(X,Y,4.0*pi*sqrt(2)*rhoDLRAp[2:(end-1),(end-1):-1:2]',norm=matplotlib.colors.LogNorm(vmin=minV, vmax=maxV))
-ax.tick_params("both",labelsize=20) 
-plt.xlabel("x", fontsize=20)
-plt.ylabel("y", fontsize=20)
-plt.title(L"$\Phi$, parallel, $r =$ "*LaTeXString(string(s.r)), fontsize=25)
-tight_layout()
-show()
-savefig("results/scalar_flux_DLRA_$(s.problem)_theta$(ϑₚ)_nx$(s.NCellsX)_N$(s.nₚₙ).png")
-
-
-## DLRA parallel 2nd order
-idxNeg = findall((rhoDLRAp2.<=0.0))
-rhoDLRAp2[idxNeg] .= NaN;
-fig = figure("parallel 2nd, log, ϑ coarse",figsize=(9,10),dpi=100)
-ax = gca()
-pcolormesh(X,Y,4.0*pi*sqrt(2)*rhoDLRAp2[2:(end-1),(end-1):-1:2]',norm=matplotlib.colors.LogNorm(vmin=minV, vmax=maxV))
+pcolormesh(X,Y,4.0*pi*sqrt(2)*rhoDLRAp2h[2:(end-1),(end-1):-1:2]',norm=matplotlib.colors.LogNorm(vmin=minV, vmax=maxV))
 ax.tick_params("both",labelsize=20) 
 plt.xlabel("x", fontsize=20)
 plt.ylabel("y", fontsize=20)
 plt.title(L"$\Phi$, parallel $2^{nd}$ order, $r = $ "*LaTeXString(string(s.r)), fontsize=25)
 tight_layout()
 show()
-savefig("results/scalar_flux_parallel2nd_$(s.problem)_rank$(s.r)_nx$(s.NCellsX)_N$(s.nₚₙ).png")
+savefig("scalar_flux_parallel2nd_$(s.problem)_rank20_nx$(s.NCellsX)_N$(s.nₚₙ).png")
 
 fig = figure("setup",figsize=(9,10),dpi=100)
 ax = fig.add_subplot(111)
@@ -150,7 +167,7 @@ plt.ylabel("y", fontsize=20)
 plt.title("lattice setup", fontsize=25)
 tight_layout()
 plt.show()
-savefig("results/setup_lattice_testcase.png")
+savefig("setup_lattice_testcase.png")
 
 
 println("main finished")
